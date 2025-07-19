18197f9ba28e21bc4aff481c01b95e125eda99bc
JustABoot <acpm.0105@gmail.com>
2025-07-19 18:30:08 -0400

    Did all exercises

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        ShowGreeting();
+
+        string user = AskForName();
+        int favNumber = GetFavoriteNumber();
+
+        int result = CalculateSquare(favNumber);
+
+        ShowFinalMessage(user, result);
+    }
+
+    static void ShowGreeting()
+    {
+        Console.WriteLine("Hello! This is a simple C# program.");
+    }
+
+    static string AskForName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int GetFavoriteNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+
+    static int CalculateSquare(int num)
+    {
+        return num * num;
+    }
+
+    static void ShowFinalMessage(string userName, int squared)
+    {
+        Console.WriteLine($"{userName}, your favorite number squared is {squared}.");
     }
 }
